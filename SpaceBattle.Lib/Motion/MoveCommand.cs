8dfa55521c3cd0554db9669d8b5f3b63bc8d8856
ivanovRoman18 <roma.ivanov149@gmail.com>
2026-04-23 16:18:19 +0600

    feat: add move command

---
+++ SpaceBattle.Lib/Motion/MoveCommand.cs
@@ -0,0 +1,27 @@
+﻿namespace SpaceBattle.Lib;
+
+public class MoveCommand : ICommand
+{
+    private readonly IMovingObject _movingObject;
+
+    public MoveCommand(IMovingObject movingObject)
+    {
+        _movingObject = movingObject;
+    }
+
+    public void Execute()
+    {
+        if (_movingObject.Position == null)
+        {
+            throw new InvalidOperationException("Cannot get position");
+        }
+
+        if (_movingObject.Velocity == null)
+        {
+            throw new InvalidOperationException("Cannot get velocity");
+        }
+
+        var newPosition = _movingObject.Position + _movingObject.Velocity;
+        _movingObject.Position = newPosition;
+    }
+}
