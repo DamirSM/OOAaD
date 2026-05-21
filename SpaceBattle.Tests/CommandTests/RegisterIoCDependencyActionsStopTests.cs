4119be3c8fc3d9aa27760dd0a0b084fa84ac5967
Lakedaimon <dkd1717301@gmail.com>
2026-05-21 20:50:08 +0600

    feat: Action.Stop

---
+++ SpaceBattle.Tests/CommandTests/RegisterIoCDependencyActionsStopTests.cs
@@ -0,0 +1,17 @@
+﻿using SpaceBattle.Lib;
+using SpaceBattle.Lib.Command;
+
+public class RegisterIoCDependencyActionsStopTests
+{
+    [Fact]
+    public void RegisterIoCDependencyActionsStop_Should_Resolve()
+    {
+        var registerCommand = new RegisterIoCDependencyActionsStop();
+        registerCommand.Execute();
+        var order = new Dictionary<string, object>();
+
+        var command = Ioc.Resolve<ICommand>("Actions.Stop", order);
+
+        Assert.NotNull(command);
+    }
+}
