22482b73172f06e7880f8ccdebc2db0a1d59bc05
Lakedaimon <dkd1717301@gmail.com>
2026-05-21 20:42:40 +0600

    feat: Action.Start

---
+++ SpaceBattle.Lib/Command/RegisterIoCDependencyActionsStart.cs
@@ -0,0 +1,10 @@
+﻿namespace SpaceBattle.Lib.Command;
+
+public class RegisterIoCDependencyActionsStart : ICommand
+{
+    public void Execute()
+    {
+        Ioc.Resolve<ICommand>("IoC.Register", "Actions.Start",
+            (Func<object[], object>)(args => new EmptyCommand()));
+    }
+}
---
+++ SpaceBattle.Tests/CommandTests/RegisterIoCDependencyActionsStartTests.cs
@@ -0,0 +1,17 @@
+﻿using SpaceBattle.Lib;
+using SpaceBattle.Lib.Command;
+
+public class RegisterIoCDependencyActionsStartTests
+{
+    [Fact]
+    public void RegisterIoCDependencyActionsStart_Should_Resolve()
+    {
+        var registerCommand = new RegisterIoCDependencyActionsStart();
+        registerCommand.Execute();
+        var order = new Dictionary<string, object>();
+
+        var command = Ioc.Resolve<ICommand>("Actions.Start", order);
+
+        Assert.NotNull(command);
+    }
+}
