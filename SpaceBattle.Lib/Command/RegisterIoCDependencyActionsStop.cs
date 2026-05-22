563585ed34cc11e693f52378a3664808759b047b
Lakedaimon <dkd1717301@gmail.com>
2026-05-23 00:00:48 +0600

    feat: add action stop realisation

--- SpaceBattle.Lib/Command/RegisterIoCDependencyActionsStop.cs
+++ SpaceBattle.Lib/Command/RegisterIoCDependencyActionsStop.cs
@@ -5,6 +5,12 @@ public class RegisterIoCDependencyActionsStop : ICommand
     public void Execute()
     {
         Ioc.Resolve<ICommand>("IoC.Register", "Actions.Stop",
-            (Func<object[], object>)(args => new EmptyCommand()));
+            (Func<object[], object>)(args =>
+            {
+                var gameObject = (IDictionary<string, object>)args[0];
+                var cmdType = (string)args[1];
+                return new StopCommand(gameObject, cmdType);
+            })
+        );
     }
 }
